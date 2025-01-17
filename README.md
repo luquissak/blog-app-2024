29b53c169e28cffee9ba9f9e956b61ab30edf7cf
Luís Fernando de Siqueira Quissak Pereira <lquissakng@gmail.com>
2025-01-17 12:05:44 -0300

    version downgrade

--- README.md
+++ README.md
@@ -10,6 +10,15 @@ genkit init
 gcloud services enable aiplatform.googleapis.com
 get-content genkit-flows-app/src/index.ts | %{$_ -replace "us-central1","southamerica-east1"}
 genkit config set analyticsOptOut true
+genkit config get analyticsOptOut
+
+
+npm uninstall @genkit-ai/evaluator 
+npm install @genkit-ai/evaluator@0.5.9
+npm install @genkit-ai/vertexai@0.5.9
+
+npm i pdf-ts
+
 ```
 
 # Run
@@ -29,14 +38,17 @@ cd genkit-flows-app && genkit start
 - Input JSON (classification) [classificationFlow](http://localhost:4000/run/%252Fflow%252FclassificationFlow):
 
 ```bash
-genkit flow:run classificationFlow '{"url": "all/pdf2/2016-02-18_Projeto_Capitalismo_e_Esquizofrenia_.pdf","content": ""}' -s 
+cd genkit-flows-app && genkit flow:run classificationFlow '{"url": "all/pdf2/2016-02-18_Projeto_Capitalismo_e_Esquizofrenia_.pdf","content": ""}' -s 
+cd genkit-flows-app && genkit flow:run classificationFlow '{"url": "all/pdf2/2016-05-23_A_informação_como_lei_da_consciência_.pdf","content": ""}' -s 
 ```
 
 # Eval
 
 ```bash
 genkit eval:flow classificationFlow --input testInputs.json
+genkit eval:flow classificationFlow --input '{"url": "all/pdf2/2016-05-23_A_informação_como_lei_da_consciência_.pdf","content": ""}'
 ```
+
 
 # GCP Console
 
@@ -46,3 +58,5 @@ genkit eval:flow classificationFlow --input testInputs.json
 
 - [Build Generative AI Apps with Firebase Genkit](https://www.cloudskillsboost.google/course_templates/1189/)
 - [Firebase Genkit](https://firebase.google.com/docs/genkit)
+- [Search Preview](https://console.cloud.google.com/gen-app-builder/locations/global/engines/all-posts2_1735054250406/preview/search?project=llm-studies)
+- [Detalhes do Blog](https://www.appsheet.com/start/1fd6c556-3fd2-4fdb-8dfc-e81bf0a81831)
--- genkit-flows-app/src/index.ts
+++ genkit-flows-app/src/index.ts
@@ -1,114 +1,36 @@
 import * as z from "zod";
-import { generate } from "@genkit-ai/ai";
 import { configureGenkit } from "@genkit-ai/core";
 import { defineFlow, startFlowsServer } from "@genkit-ai/flow";
 import { vertexAI } from "@genkit-ai/vertexai";
 import { gemini15Flash } from "@genkit-ai/vertexai";
-import { defineDotprompt } from "@genkit-ai/dotprompt";
-import { defineTool } from "@genkit-ai/ai";
-import { Storage } from "@google-cloud/storage";
-import { readFile } from "fs/promises";
-import { pdfToText } from "pdf-ts";
 import { genkitEval, GenkitMetric } from "@genkit-ai/evaluator";
+import { contentSchema } from "./lib/genkit/schemas";
+import {
+  classificationPrompt,
+  argumentationPrompt,
+} from "./lib/genkit/prompts";
+import { getPostTextTool } from "./lib/genkit/tools";
 
 configureGenkit({
   plugins: [
     vertexAI({ location: "southamerica-east1" }),
-//    genkitEval({
-//      judge: gemini15Flash,
-//      metrics: [
-//        GenkitMetric.FAITHFULNESS,
-//        GenkitMetric.MALICIOUSNESS,],
-//    }),
+    genkitEval({
+      judge: gemini15Flash,
+      metrics: [GenkitMetric.FAITHFULNESS, GenkitMetric.MALICIOUSNESS],
+    }),
   ],
   logLevel: "debug",
   enableTracingAndMetrics: true,
+  //  telemetry: {
+  //    instrumentation: 'firebase',
+  //    logger: 'firebase',
+  //    },
 });
-export const ContentSchema = z.object({
-  url: z.string(),
-  content: z.string(),
-});
-
-const getPostTextTool = defineTool(
-  {
-    name: "getPostTextTool",
-    description: "Use this tool to get the post content",
-    inputSchema: ContentSchema,
-    outputSchema: z.string(),
-  },
-  async (input) => {
-    const storage = new Storage();
-    const postFile = storage.bucket("blog-files-2024").file(input.url);
-    console.log(postFile.name);
-    const contents = await postFile.download({ destination: "test.pdf" });
-    const content = await readFile("./test.pdf");
-    const text = await pdfToText(content);
-    return text;
-  }
-);
-
-export const classificationPrompt = defineDotprompt(
-  {
-    name: "classificationPrompt",
-    model: gemini15Flash,
-    input: { schema: ContentSchema },
-    output: { format: "text" },
-    config: { temperature: 0 },
-    //    tools: [getPostTextTool],
-  },
-  `
-  Você é um assistente de classificação de postagens de um blog de filosofia.
-  Dado o conteúdo de uma postagem, sua tarefa é descobrir a qual categoria ela pertence na lista de categorias de postagens fornecida abaixo.
-
-  Antropologia
-  Ciência
-  Crônica
-  Educação
-  Epistemologia
-  Ética
-  Liberdade
-  Linguagem
-  Marxismo
-  Mente
-  Ontologia Social
-  Poesia
-  Política
-  Psicanálise
-  Tecnologia
-
-  Classifique:
-  {{content}}
-  `
-);
-
-export const argumentationPrompt = defineDotprompt(
-  {
-    name: "argumentationPrompt",
-    model: gemini15Flash,
-    input: { schema: ContentSchema },
-    output: { format: "text" },
-    config: { temperature: 0 },
-  },
-  `
-  Liste os argumentos do texto, com os nomes do autores, um título para cada argumento, uma síntese de cada argumento e o nome do arquivo.
-  Procure não repetir argumentos.
-  Se não for possível identificar argumentos gere apenas uma linha colocando "não encontrado" nas 3 primeiras colunas, mas coloque o nome do arquivo e finalize.
-  Deve sempre haver 4 colunas preenchidas: uma com o nome do autor, outra com o título do argumento, a terceira com a síntese do argumento e a quarta com o nome do arquivo, mesmo que o autor se repita.
-  Use o separador | entre as colunas.
-  Se houver mais de um argumento, liste-os em linhas separadas.
-  Toda a linha deve ter o nome do arquivo, mesmo que repetido.
-  Se não for possível identificar o autor, considere que é "Luis Quissak".
-  Não repita linhas.
-
-  Texto:
-  {{content}}
-  `
-);
 
 export const classificationFlow = defineFlow(
   {
     name: "classificationFlow",
-    inputSchema: ContentSchema,
+    inputSchema: contentSchema,
     outputSchema: z.string(),
   },
   async (input) => {
@@ -123,7 +45,7 @@ export const classificationFlow = defineFlow(
 export const argumentationFlow = defineFlow(
   {
     name: "argumentationFlow",
-    inputSchema: ContentSchema,
+    inputSchema: contentSchema,
     outputSchema: z.string(),
   },
   async (input) => {
